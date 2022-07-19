7670a614578f26c420c003945d39e1e9fc996abc
abhishek lohani <alohani1308@gmail.com>
2022-07-19 16:18:54 +0530

    Adding Portal for overlay

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+//import Cart from "./Components/Cart/CArt";
+import { Fragment } from "react";
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
-    <div>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals/>
       </main>
-    </div>
+    </Fragment>
   );
 }
 
