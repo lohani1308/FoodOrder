dbb5532e22f2510299aca28ae78198ee00708b0d
abhishek lohani <alohani1308@gmail.com>
2022-07-27 13:33:30 +0530

    adding context and making cart functioning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 //import Cart from "./Components/Cart/CArt";
-import { Fragment, useState } from "react";
+import {  useState } from "react";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
 
@@ -18,13 +19,13 @@ function App() {
 
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsshown && <Cart onClose={hideCartHandler}/>}
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
